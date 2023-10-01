bebe204d481cf106d89ecda7dffee9d11024f96f
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-01 14:06:26 -0700

    Add pointer vars to primary and button elements

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// pointer variables for primary page element IDs
+var welcomeEl = document.getElementById("welcome-wrapper");
+var quizEl = document.getElementById("quiz-wrapper");
+var doneEl = document.getElementById("all-done-wrapper");
+var scoresEl = document.getElementById("high-scores-wrapper");
+
+// pointer variables for button elements
+var showScoresButton = document.getElementById("high-scores-button");
+var startButton = document.getElementById("start-quiz-button");
+var response1Button = document.getElementById("response1");
+var response2Button = document.getElementById("response2");
+var response3Button = document.getElementById("response3");
+var response4Button = document.getElementById("response4");
+var submitButton = document.getElementById("initials-submit-button");
+var backButton = document.getElementById("go-back");
+var clearButton = document.getElementById("clear-high-scores");
+
+// other pointer variables
+
+// clearButton.setAttribute()
+// global constants
+
+
+// global variables
+
+
+function hideElement(element) {
+  element.setAttribute("display", "none");
+}
+
