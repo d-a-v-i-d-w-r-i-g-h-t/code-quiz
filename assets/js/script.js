916356efd4a23f6106956715482405be566732a8
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-01 23:54:00 -0700

    Add Home button, remove Back button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@ var scoresEl = document.getElementById("high-scores-wrapper");
 
 // pointer variables for button elements
 var showScoresButton = document.getElementById("high-scores-button");
+var homeButton = document.getElementById("home-button");
 var startButton = document.getElementById("start-quiz-button");
 var response1Button = document.getElementById("response1");
 var response2Button = document.getElementById("response2");
 var response3Button = document.getElementById("response3");
 var response4Button = document.getElementById("response4");
 var submitButton = document.getElementById("initials-submit-button");
-var backButton = document.getElementById("go-back");
 var clearButton = document.getElementById("clear-high-scores");
 
 // other pointer variables
@@ -25,20 +25,36 @@ var response1El = document.getElementById("response1");
 var response2El = document.getElementById("response2");
 var response3El = document.getElementById("response3");
 var response4El = document.getElementById("response4");
-
+var myScoreSpan = document.getElementById("myScore");
 
 // global constants
 const quizDuration = 63; // quiz duration in seconds
 const penalty = 10; // seconds removed from timer for each incorrect response
-const numResponses = 4; // each quiz question is multiple choice with four possible answers
 const secondsPerMinute = 60; // 60 seconds in a minute
 const leadingZero = "0"; // used to pad timer seconds to ensure two digit seconds value
 const ten = 10; // used to check if leading zero is required
+const blank = " ________"; // used in responses
+const numResponses = 4; // each quiz question is multiple choice with four possible answers
+const one = "1. "; // prepended to response 1
+const two = "2. "; // prepended to response 2
+const three = "3. "; // prepended to response 3
+const four = "4. "; // prepended to response 4
 
 // global variables
 var timeRemaining = 0;
 var quizComplete = false;
 var questionNumber = 0;
+var questions = {
+  1: {
+    question: `Arrays in JavaScript can be used to store ${blank}.`,
+    response1: "",
+    response2: "",
+    response3: "",
+    response4: "",
+    answer: 4,
+  }
+}
+var highScores; // OBJECT VAR
 
 
 function startTimer() {
@@ -153,8 +169,8 @@ function correctResponse() {
 function nextQuestion() {
   questionNumber++;
   
-  if (questionNumber > NUMBER_OF_QUESTIONS) {
-    endQuiz();
+  if (questionNumber > ten) {
+    endQuiz(); // HOW MANY QUESTIONS? GET THIS FROM THE QUESTION OBJECT VAR
   }
 
   // DISPLAY QUESTION NUMBER questionNumber
@@ -164,6 +180,10 @@ function nextQuestion() {
 // event listeners
 showScoresButton.addEventListener("click", function() {
   scoresMode();
+});
+
+homeButton.addEventListener("click", function() {
+  goHome();
 });
 
 startButton.addEventListener("click", function() {
@@ -190,19 +210,17 @@ submitButton.addEventListener("click", function() {
   submitInitials();
 });
 
-backButton.addEventListener("click", function() {
-  goBackHome();
-});
-
 clearButton.addEventListener("click", function() {
   clearHighScores();
 });
 
-function goBackHome() {
+function goHome() {
+  quizComplete = true;
   welcomeMode();
 }
 
 function startQuiz() {
+  quizComplete = false;
   quizMode();
   startTimer();
   questionNumber = 0;
@@ -211,6 +229,7 @@ function startQuiz() {
 
 function endQuiz() {
   quizComplete = true;
+  myScoreSpan.textContent = timeRemaining; // score is equal to time remaining
   doneMode();
 }
 
@@ -235,10 +254,10 @@ function welcomeMode() {
   hideElement(scoresEl);
 
   showElement(showScoresButton);
+  hideElement(homeButton);
   hideElement(timerEl);
 }
-// TIMER NEEDS TO BE ALWAYS TOP RIGHT ALIGNED
-// MAYBE SCORES BUTTON CAN BE VISIBILITY:HIDDEN INSTEAD OF DISPLAY: NONE
+
 function quizMode() {
   hideElement(welcomeEl);
   showElement(quizEl);
@@ -246,6 +265,7 @@ function quizMode() {
   hideElement(scoresEl);
 
   hideElement(showScoresButton);
+  showElement(homeButton);
   showElement(timerEl);
 }
 // MAYBE HOME BUTTON INSTEAD OF SHOW SCORES BUTTON IN TOP LEFT ON THIS PAGE
@@ -255,7 +275,8 @@ function doneMode() {
   showElement(doneEl);
   hideElement(scoresEl);
 
-  showElement(showScoresButton);
+  hideElement(showScoresButton);
+  showElement(homeButton);
   showElement(timerEl);
 }
 
@@ -265,9 +286,8 @@ function scoresMode() {
   hideElement(doneEl);
   showElement(scoresEl);
 
-  // *** COULD HAVE BACK BUTTON IN THE PLACE OF   *** //
-  // *** TOP LEFT SHOW SCORES BUTTON IN THIS MODE *** //
   hideElement(showScoresButton);
+  showElement(homeButton);
   hideElement(timerEl);
 }
 
